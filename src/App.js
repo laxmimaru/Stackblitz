d76059805e3a1d5f71521ba30eea4b1038399728
laxmimaru <laxmimaru28@gmail.com>
2023-12-08 23:06:11 +0530

    changes for input color change

--- src/App.js
+++ src/App.js
@@ -8,25 +8,23 @@ export default function App() {
   const changeColor = () => {
     console.log('changeColor');
     console.log(255 * Math.random());
-    setIpStyle(255 * Math.random());
+    setIpStyle(255 * Math.random());        
   };
 
-  const getPosts=async ()=>{
-    const posts = await fetch('https://jsonplaceholder.typicode.com/posts')
-    console.log('posts = ',posts)
-  }
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <input
         type="text"
-        style={{ color: `rgb(${(ipStyle, 255, 255)})` }}
-        value="test demo"
-      />{' '}
+        style={{ color: `rgb(${ipStyle},${ipStyle},${ipStyle}` }}
+        value="test demo testings"
+      />
       <br />
       <br />
+      <p style={{ color: `rgb(${ipStyle},${ipStyle},${ipStyle}` }}>test</p>
       <button onClick={() => changeColor()}>submit</button>
-      <p style={{ color: 'rgb(211,250,210' }}>test</p>
+      
     </div>
   );
 }
